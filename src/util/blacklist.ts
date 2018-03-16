2aa3c4ee2eecdf04f889dc3f87726c44b2b2493e
TanninOne <sherb@gmx.net>
2018-03-16 13:48:04 +0100

    ignore file conflicts on meta.ini and mod.manifest files

--- src/util/blacklist.ts
+++ src/util/blacklist.ts
@@ -3,9 +3,12 @@ import minimatch = require('minimatch');
 const blacklist = [
   'fomod\\*',
   'readme*',
+  'meta.ini',       // Mod Organizer
+  'mod.manifest',   // Kingdom Come: Deliverance
 ];
 
 function isBlacklisted(filePath: string): boolean {
+  // TODO: this could become reaaaaly slow as the blacklist gets larger...
   return blacklist.find(pattern => minimatch(filePath, pattern)) !== undefined;
 }
 
