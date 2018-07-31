0fb8fdb45c119839e562873002ccf9fd58e93cfa
TanninOne <seppleviathan@gmx.de>
2018-07-31 11:48:36 +0200

    fixed: exception if mod has no attributes

--- src/selectors.ts
+++ src/selectors.ts
@@ -18,7 +18,7 @@ export const enabledModKeys = createSelector(currentGameMods, modState, (mods, m
   const res: IModLookupInfo[] = [];
 
   Object.keys(mods || {}).forEach(modId => {
-    const attributes = mods[modId].attributes;
+    const attributes = mods[modId].attributes || {};
     if (util.getSafe(modStateIn, [modId, 'enabled'], false)
         && (attributes['fileMD5'] || attributes['fileName']
             || attributes['logicalFileName'] || attributes['name'])) {
