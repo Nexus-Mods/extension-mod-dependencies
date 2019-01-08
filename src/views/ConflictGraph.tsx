ad968272e97eb0830d57067bff902e83a26af9a2
TanninOne <seppleviathan@gmx.de>
2019-01-08 19:28:11 +0100

    replaced conflict graph with cytoscape
    apart from the visual improvements this now supports highlighting individual cycles and flipping rules instead of removing them entirely
    
    TODO: updating once a change was made is slow and doesn't always seem to happen

--- src/views/ConflictGraph.tsx
+++ src/views/ConflictGraph.tsx
@@ -1,18 +1,20 @@
 import { IBiDirRule } from '../types/IBiDirRule';
 import { IConflict } from '../types/IConflict';
-import Graph, { IGraphLinkSpec } from '../util/graph';
+import genGraphStyle from '../util/genGraphStyle';
 
 import { setEditCycle } from '../actions';
 
+import GraphView, { IGraphElement, IGraphSelection } from './GraphView';
+
 import { IRule } from 'modmeta-db';
 import * as React from 'react';
-import { Button, Modal } from 'react-bootstrap';
-import { withFauxDOM } from 'react-faux-dom';
+import { Button } from 'react-bootstrap';
 import { translate } from 'react-i18next';
 import { connect } from 'react-redux';
 import * as Redux from 'redux';
 import { ThunkDispatch } from 'redux-thunk';
-import { actions, ComponentEx, selectors, types, util } from 'vortex-api';
+import { actions, ComponentEx, Modal, selectors, types, util, ContextMenu } from 'vortex-api';
+const { Usage } = require('vortex-api');
 
 interface ILocalState {
   modRules: IBiDirRule[];
@@ -51,18 +53,53 @@ type IProps = IConflictGraphProps & IConnectedProps & IActionProps & IFauxProps;
 interface IComponentState {
   highlighted: { source: string, target: string };
   counter: number;
+
+  context: {
+    x: number,
+    y: number,
+    selection?: IGraphSelection,
+  };
+
+  elements: { [id: string]: IGraphElement };
 }
 
 class ConflictGraph extends ComponentEx<IProps, IComponentState> {
-  private mCycle: { gameId: string, modIds: string[] };
-  private mRules: IBiDirRule[];
-  private mGraph: Graph;
-  private mHighlighted: { source: string, target: string };
   private mProxy: any;
+  private mGraphRef: GraphView;
+  private mContextTime: number;
+
+  private contextNodeActions = [
+    {
+      title: 'Highlight Cycle',
+      show: true,
+      action: () => this.highlightCycle(),
+    },
+   ];
+
+   private contextEdgeActions = [
+    {
+      title: 'Flip Rule',
+      show: true,
+      action: () => this.flipRule(),
+    },
+    {
+      title: 'Remove Rule',
+      show: true,
+      action: () => this.removeRule(),
+    },
+   ];
+
+  private contextBGActions = [
+    {
+      title: 'Layout',
+      show: true,
+      action: () => this.mGraphRef.layout(),
+    },
+  ];
 
   constructor(props: IProps) {
     super(props);
-    this.initState({ highlighted: undefined, counter: 0 });
+    this.initState({ highlighted: undefined, counter: 0, context: undefined, elements: undefined });
     // TODO: horrible hack, just to get this to update when mod rules change
     this.mProxy = {
       setState: () => {
@@ -72,8 +109,6 @@ class ConflictGraph extends ComponentEx<IProps, IComponentState> {
   }
 
   public componentDidMount() {
-    const { width, height, nodeDistance, nodeRadius, connectFauxDOM } = this.props;
-    this.mGraph = new Graph(width, height, nodeDistance, nodeRadius, connectFauxDOM);
     this.nextState.highlighted = undefined;
     (this.props.localState as any).attach(this.mProxy);
   }
@@ -91,25 +126,44 @@ class ConflictGraph extends ComponentEx<IProps, IComponentState> {
         || (this.props.mods !== newProps.mods)
         || (this.props.localState.modRules !== newProps.localState.modRules)
         || (this.props.editCycle !== newProps.editCycle)) {
-      this.props.animateFauxDOM(60000);
-      this.mGraph.reposition(() => {
-        this.props.stopAnimatingFauxDOM();
-      });
+      this.updateGraph(newProps);
     }
   }
 
   public render(): JSX.Element {
     const { t, editCycle, mods } = this.props;
-    if (this.mGraph === undefined) {
+    if (editCycle === undefined) {
       return null;
     }
-    this.updateGraph(this.props);
+
+    let contextActions;
+    if (this.state.context !== undefined) {
+      contextActions = (this.state.context.selection !== undefined)
+        ? (this.state.context.selection.id !== undefined)
+          ? this.contextNodeActions
+          : this.contextEdgeActions
+        : this.contextBGActions;
+    }
+
     return (
-      <Modal show={editCycle !== undefined} onHide={this.close}>
+      <Modal id='conflict-graph-dialog' show={editCycle !== undefined} onHide={this.close}>
         <Modal.Header><Modal.Title>{t('Cycle')}</Modal.Title></Modal.Header>
         <Modal.Body>
-          {t('Click a connection to remove the rule')}
           {this.renderGraph()}
+          <ContextMenu
+            position={this.state.context}
+            visible={this.state.context !== undefined}
+            onHide={this.hideContext}
+            instanceId='42'
+            actions={contextActions}
+          />
+          <Usage infoId='conflicting-mods' persistent>
+            <div>{t('This screen shows a cluster of mods that form one or more cycles.')}</div>
+            <div>{t('If there are too many connections you can highlight a single cycle by right-clicking on one of the mods.')}</div>
+            <div>{t('You can resolve a cycle by either removing or flipping one rule, so if you have a cycle '
+              + 'A->-B->-C->-A you could flip the arrow between C and A to get A->-B->-C-<-A which is no longer a cycle. Repeat this until '
+              + 'there are no cycles left.')}</div>
+          </Usage>
         </Modal.Body>
         <Modal.Footer>
           <Button onClick={this.close}>{t('Close')}</Button>
@@ -118,14 +172,77 @@ class ConflictGraph extends ComponentEx<IProps, IComponentState> {
     );
   }
 
+  private openContext = (x: number, y: number, selection: IGraphSelection) => {
+    if ((selection !== undefined) && selection.readonly) {
+      return;
+    }
+    this.nextState.context = { x, y, selection };
+    this.mContextTime = Date.now();
+  }
+
+  private hideContext = () => {
+    if (Date.now() - this.mContextTime < 20) {
+      // workaround: somehow I can't prevent the event that opens the context menu from being
+      // propagated up, which will be picked up as close event
+      return;
+    }
+    this.nextState.context = undefined;
+  }
+
+  private getThemeSheet(): CSSStyleRule[] {
+    // tslint:disable-next-line:prefer-for-of
+    for (let i = 0; i < document.styleSheets.length; ++i) {
+      if ((document.styleSheets[i].ownerNode as any).id === 'theme') {
+        return Array.from((document.styleSheets[i] as any).rules);
+      }
+    }
+    return [];
+  }
+
+  private updateGraph(props: IProps) {
+    const { editCycle, mods, localState } = props;
+
+    if (editCycle === undefined) {
+      this.nextState.elements = {};
+      return;
+    }
+
+    const links = (modId: string): string[] => {
+      return localState.modRules
+        .filter(rule => (rule.type === 'after') && util.testModReference(mods[modId], rule.source))
+        .map(rule => editCycle.modIds.filter(refId =>
+            util.testModReference(mods[refId], rule.reference)))
+        .reduce((prev, refs) => [...prev, ...refs], []);
+    }
+
+    const elements: { [id: string]: IGraphElement } = editCycle.modIds.reduce((prev, modId) => {
+      prev[modId] = {
+        title: util.renderModName(mods[modId]), connections: links(modId),
+        class: `conflictnode`, readonly: false
+      };
+      return prev;
+    },  {});
+
+    this.nextState.elements = elements;
+  }
+
   private renderGraph(): JSX.Element {
+    const { elements } = this.state;
+    const sheet = this.getThemeSheet();
+
     return (
-      <div>
-        <div className='mod-conflict-graph'>
-          {this.props.chart}
-        </div>
-      </div>
+      <GraphView
+        className='conflict-graph'
+        elements={elements}
+        visualStyle={genGraphStyle(sheet)}
+        onContext={this.openContext}
+        ref={this.setGraphRef}
+      />
     );
+  }
+
+  private setGraphRef = (ref: GraphView) => {
+    this.mGraphRef = ref;
   }
 
   private close = () => {
@@ -133,74 +250,55 @@ class ConflictGraph extends ComponentEx<IProps, IComponentState> {
     onClose();
   }
 
-  private updateGraph(props: IProps) {
-    const { editCycle, localState, mods, onRemoveRule } = props;
-    const { highlighted } = this.state;
-    if (editCycle === undefined) {
+  private highlightCycle = () => {
+    const { id } = this.state.context.selection;
+
+    this.mGraphRef.highlightCycle(id);
+  }
+
+  private flipRule = () => {
+    const { editCycle, localState, mods, onAddRule, onRemoveRule } = this.props;
+    const { selection } = this.state.context;
+
+    const bidirRule = localState.modRules.find(rule =>
+          (rule.type === 'before')
+          && util.testModReference(mods[selection.source], rule.source)
+          && util.testModReference(mods[selection.target], rule.reference));
+    if (bidirRule === undefined) {
       return;
     }
 
-    let change: boolean = false;
-    if (this.mCycle !== editCycle) {
-      const nodes = editCycle.modIds.map(modId =>
-        ({ id: modId, name: util.renderModName(props.mods[modId]) }));
-      this.mGraph.setNodes(nodes);
-      this.mCycle = editCycle;
-      change = true;
-    }
+    const sourceId = bidirRule.original ? selection.source : selection.target;
+    const rule: IRule = {
+      type: bidirRule.original ? 'before' : 'after',
+      reference: bidirRule.original ? bidirRule.reference : bidirRule.source,
+    };
 
-    if ((this.mRules !== localState.modRules)
-      || (this.mHighlighted !== highlighted)) {
-      const links: IGraphLinkSpec[] = editCycle.modIds.reduce((prev: IGraphLinkSpec[], modId: string) => {
-        localState.modRules
-          .filter(rule => (rule.type === 'after')
-            && util.testModReference(mods[modId], rule.source))
-          .forEach(rule => {
-            const otherMods: string[] = editCycle.modIds.filter(refId =>
-              util.testModReference(mods[refId], rule.reference));
-            otherMods.forEach(otherMod => {
-              prev.push({
-                source: modId,
-                target: otherMod,
-                highlight: (highlighted !== undefined)
-                  && (highlighted.source === modId) && (highlighted.target === otherMod),
-              });
-            });
-          });
-        return prev;
-      }, []);
+    onRemoveRule(editCycle.gameId, sourceId, rule);
 
-      this.mGraph.setLinks(links, (source: string, target: string) => {
-        const bidirRule = localState.modRules.find(rule =>
-          rule.type === 'after'
-          && util.testModReference(mods[source], rule.source)
-          && util.testModReference(mods[target], rule.reference));
+    // swap before <-> after, then add that exact same rule again
+    rule.type = bidirRule.original ? 'after' : 'before';
+    onAddRule(editCycle.gameId, sourceId, rule);
+  }
+
+  private removeRule = () => {
+    const { editCycle, localState, mods, onRemoveRule } = this.props;
+    const { selection } = this.state.context;
+
+    const bidirRule = localState.modRules.find(rule =>
+          rule.type === 'before'
+          && util.testModReference(mods[selection.source], rule.source)
+          && util.testModReference(mods[selection.target], rule.reference));
         if (bidirRule === undefined) {
           return;
         }
-        const sourceId = bidirRule.original ? source : target;
+        const sourceId = bidirRule.original ? selection.source : selection.target;
         const remRule: IRule = {
-          type: bidirRule.original ? 'after' : 'before',
+          type: bidirRule.original ? 'before' : 'after',
           reference: bidirRule.original ? bidirRule.reference : bidirRule.source,
         };
-        onRemoveRule(editCycle.gameId, sourceId, remRule);
-      }, (source: string, target: string, highlight: boolean) => {
-        this.nextState.highlighted = highlight
-          ? { source, target }
-          : undefined;
-      });
 
-      this.mRules = localState.modRules;
-      this.mHighlighted = highlighted;
-      change = true;
-    }
-
-    if (change) {
-      props.animateFauxDOM(60000);
-      this.mGraph.reposition(() => {
-        props.stopAnimatingFauxDOM();
-      });
-    }
+    onRemoveRule(editCycle.gameId, sourceId, remRule)
   }
 }
 
@@ -234,4 +332,4 @@ function mapDispatchToProps(dispatch: ThunkDispatch<any, null, Redux.Action>): I
 
 export default translate(['common', 'dependency-manager'], {wait: false})(
   connect(mapStateToProps, mapDispatchToProps)(
-    withFauxDOM(ConflictGraph))) as React.ComponentClass<IConflictGraphProps>;
+    ConflictGraph)) as React.ComponentClass<IConflictGraphProps>;
---
+++ src/views/GraphView.tsx
@@ -0,0 +1,249 @@
+import * as cytoscape from 'cytoscape';
+import * as coseBilkent from 'cytoscape-cose-bilkent';
+import * as React from 'react';
+import { util } from 'vortex-api';
+
+(cytoscape as any).use(coseBilkent);
+
+const MAX_COLUMNS = 10;
+
+export interface IGraphElement {
+  title: string;
+  class: string;
+  connections: string[];
+  readonly?: boolean;
+}
+
+export interface IGraphSelection {
+  source?: string;
+  target?: string;
+  id?: string;
+  readonly?: boolean;
+}
+
+export interface IGraphViewProps {
+  elements: { [id: string]: IGraphElement };
+  className: string;
+  style?: any;
+  visualStyle: cytoscape.Stylesheet[];
+  onContext: (x: number, y: number, selection: IGraphSelection) => void;
+}
+
+function san(input: string): string {
+  return input.replace(/[ &()]/g, '_');
+}
+
+class GraphView extends React.Component<IGraphViewProps, {}> {
+  private mGraph: cytoscape.Core;
+  private mLayout: cytoscape.LayoutManipulation;
+  private mMousePos: { x: number, y: number } = { x: 0, y: 0 };
+  private mLastProps: IGraphViewProps;
+
+  constructor(props: IGraphViewProps) {
+    super(props);
+
+    this.mLastProps = props;
+  }
+
+  public componentWillReceiveProps(newProps: IGraphViewProps) {
+    if (newProps.elements !== this.mLastProps.elements) {
+      const changed = (util as any).objDiff(this.mLastProps.elements, newProps.elements);
+      // bit of a hack because we got componentWillReceiveProps trigger twice with the same
+      // props which causes an error further down the line as we try to update the graph
+      this.mLastProps = newProps;
+
+      Object.keys(changed).forEach(id => {
+        if (id[0] === '+') {
+          const zoom = 1.0 / this.mGraph.zoom();
+          // node added
+          this.mGraph.add({
+            data: { id: san(id.slice(1)), title: changed[id].title },
+            classes: changed[id].class,
+            position: this.mMousePos,
+          });
+          const connections = changed[id].connections;
+          Object.keys(connections || []).forEach(refId => {
+            const from = san(id.slice(1));
+            const to = san(connections[refId]);
+            this.mGraph.add({
+              data: {
+                id: `${to}-to-${from}`,
+                source: to,
+                sourceOrig: connections[refId],
+                target: from,
+                targetOrig: id.slice(1),
+              },
+              classes: newProps.elements[id].class,
+            });
+          });
+        } else if (id[0] === '-') {
+          // node removed
+          this.mGraph.remove('#' + san(id.slice(1)));
+        } else {
+          // updated classes
+          const nodeId = san(id);
+          if (this.props.elements[id].class !== newProps.elements[id].class) {
+            this.mGraph.$(`node#${nodeId}, edge[target = "${nodeId}"]`)
+              .removeClass(this.props.elements[id].class)
+              .addClass(newProps.elements[id].class);
+          }
+          // node content changed
+          Object.keys(changed[id].connections || [])
+            .sort((lhs, rhs) => {
+              if (lhs[0] !== rhs[0]) {
+                return lhs[0] === '-' ? -1 : 1;
+              } else {
+                return lhs.localeCompare(rhs);
+              }
+            })
+            .forEach(refId => {
+            const from = san(id);
+            const to = san(changed[id].connections[refId]);
+            const connId = `${to}-to-${from}`;
+            if (refId[0] === '-') {
+              this.mGraph.remove('#' + connId);
+            } else if (refId[0] === '+') {
+              this.mGraph.add({
+                data: {
+                  id: connId,
+                  source: to,
+                  sourceOrig: changed[id].connections[refId],
+                  target: from,
+                  targetOrig: id,
+                },
+                classes: newProps.elements[id].class,
+              });
+            }
+          });
+        }
+      });
+      this.mLayout.run();
+    }
+  }
+
+  public layout() {
+    this.mLayout.run();
+  }
+
+  public highlightCycle(nodeId: string) {
+    // we find the cycle by using a pathfinding algorithm
+    // Since the algorithms are implemented to stop immediately when start and goal
+    // are the same node, we have to identify the nodes reachable from the selection
+    // and do a search from each.
+
+    const followers = this.mGraph
+      .$(`#${san(nodeId)}`)
+      .outgoers()
+      .filter(ele => ele.group() === 'nodes');
+
+    const firstCycle = followers.reduce((prev, node) => {
+      if (prev === undefined) {
+        const path = this.mGraph.elements().aStar({
+          root: `#${san(node.id())}`,
+          goal: `#${san(nodeId)}`,
+          directed: true
+        });
+        if (path.found) {
+          prev = path.path;
+        }
+      }
+      return prev;
+    }, undefined);
+    
+    if (firstCycle !== undefined) {
+      // unhighlight previous cycle if necessary
+      this.mGraph.elements()
+        .addClass('cycle-hidden')
+        .removeClass('cycle-highlight');
+
+      // highlight edge to the node from which the cycle starts
+      this.mGraph.$(`#${san(nodeId)}-to-${san(firstCycle[0].id())}`)
+        .removeClass('cycle-hidden')
+        .addClass('cycle-highlight');
+      // highlight all other edges in the cycle
+      firstCycle.filter(iter => iter.group() === 'edges')
+        .removeClass('cycle-hidden')
+        .addClass('cycle-highlight');
+    }
+  }
+
+  public render(): JSX.Element {
+    const { className, style } = this.props;
+
+    return <div ref={this.setRef} className={className} style={style} />;
+  }
+
+  private setRef = (ref: HTMLDivElement) => {
+    const { elements, visualStyle } = this.props;
+    if (ref === null) {
+      (this.mGraph as any).off('cxttap', this.handleContext);
+      this.mGraph = undefined;
+      return;
+    }
+    this.mGraph = cytoscape({
+      container: ref,
+      style: visualStyle,
+      minZoom: 0.33,
+      maxZoom: 3,
+      boxSelectionEnabled: false,
+    });
+    this.addElements(elements);
+    this.mGraph.resize();
+    this.mGraph.center();
+    this.mLayout = this.mGraph.layout({
+      name: 'cose-bilkent',
+      nodeDimensionsIncludeLabels: true,
+      randomize: false,
+    } as any);
+    this.mLayout.run();
+    (this.mGraph as any).on('cxttap', this.handleContext);
+  }
+
+  private handleContext = (evt: cytoscape.EventObject) => {
+    let selection;
+    if (evt.target.data !== undefined) {
+      const data = evt.target.data();
+      if ((data.title === undefined) && (data.source === undefined)) {
+        // an item was hit, but neither a node nor an edge. Probably the edge handle
+        return;
+      }
+      selection = (data.source !== undefined)
+        ? { source: data.sourceOrig, target: data.targetOrig, readonly: data.readonly }
+        : { id: data.title, readonly: data.readonly };
+    }
+    this.mMousePos = evt.position;
+    this.props.onContext(evt.renderedPosition.x, evt.renderedPosition.y, selection);
+  }
+
+  private addElements(elements: { [id: string]: IGraphElement }) {
+    const width = MAX_COLUMNS;
+    const distance = (this.mGraph.width() / width) * 2;
+    this.mGraph
+      .add(Object.keys(elements).reduce((prev, id: string, idx: number) => {
+        const ele = elements[id];
+        const row = Math.floor(idx / width);
+        const pos = (row % 2 === 0) ? (idx % width) : width - (idx % width);
+        prev.push({
+          data: { id: san(id), title: ele.title, readonly: ele.readonly },
+          classes: ele.class,
+          position: { x: pos * distance, y: row * distance },
+        });
+        (ele.connections || []).forEach(conn => {
+          prev.push({
+            data: {
+              id: san(`${san(conn)}-to-${san(id)}`),
+              target: san(id),
+              source: san(conn),
+              targetOrig: id,
+              sourceOrig: conn,
+              readonly: ele.readonly,
+            },
+            classes: ele.class,
+          });
+        });
+        return prev;
+      }, []));
+  }
+}
+
+export default GraphView;
