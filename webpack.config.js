02574b69496f33077b1aa24e9607f494f6682064
TanninOne <seppleviathan@gmx.de>
2022-01-18 13:03:47 +0100

    updated webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 let webpack = require('vortex-api/bin/webpack').default;
 
-const config = webpack('mod-dependency-manager', __dirname, 4);
+const config = webpack('mod-dependency-manager', __dirname, 5);
 
 config.externals.turbowalk = 'turbowalk';
 
